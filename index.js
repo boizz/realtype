143cca8b58a2446bbaffc49402ccef3d9853f131
Laughing Gor <bon.hoo@hotmail.com>
2017-05-10 09:45:52 +0800

    support ie

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = function realType(obj) {
   var class2type = {}
-  'Boolean Number String Function Array Date RegExp Object Error'.split(' ').map((e) => {
+  'Boolean Number String Function Array Date RegExp Object Error'.split(' ').forEach(function (e) {
     class2type[`[object ${e}]`] = e.toLowerCase()
   })
   if ( obj == null ) {
